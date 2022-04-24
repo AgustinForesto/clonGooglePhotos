cf737aa749a512e177b3326942d7ed76135d9f64
AgustinForesto <agustinsanchez099@gmail.com>
2022-04-24 01:37:19 -0300

    Creacion de servidor y Configurar middleaware

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-console.log("que onda logi");
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+import { join } from "path";
+export const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(join(__dirname, "../public/")));
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET as string,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.get("/", (req: Request, res: Response) => { 
+    res.send("Que ondaaa desde el server(?")
+});
+
+app.listen(3000, () => {
+    console.log("Servidor en el puerto 3000");
+});
